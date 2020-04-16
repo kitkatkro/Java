cf9fd3ca424e0b93ee3339785e98af3694533457
Ekaterina Kropotova <kropotova.kate@gmail.com>
2020-04-16 10:10:31 +0300

    Homework_04

---
+++ Homework_04/src/ClientGUI.java
@@ -0,0 +1,120 @@
+package ru.gb.jtwo.d_lesson.online;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class ClientGUI extends JFrame implements ActionListener, Thread.UncaughtExceptionHandler {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 300;
+
+    private final JTextArea log = new JTextArea();
+    private final JPanel panelTop = new JPanel(new GridLayout(2, 3));
+    private final JTextField tfIPAddress = new JTextField("127.0.0.1");
+    private final JTextField tfPort = new JTextField("8189");
+    private final JCheckBox cbAlwaysOnTop = new JCheckBox("Always on top", true);
+    private final JTextField tfLogin = new JTextField("ivan");
+    private final JPasswordField tfPassword = new JPasswordField("123");
+    private final JButton btnLogin = new JButton("Login");
+
+    private final JPanel panelBottom = new JPanel(new BorderLayout());
+    private final JButton btnDisconnect = new JButton("<html><b>Disconnect</b></html>");
+    private final JTextField tfMessage = new JTextField();
+    private final JButton btnSend = new JButton("Send");
+
+    private final JList<String> userList = new JList<>();
+    BufferedWriter bufferedWriter;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ClientGUI();
+            }
+        });
+    }
+
+    ClientGUI() {
+        Thread.setDefaultUncaughtExceptionHandler(this);
+        // writerOn();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(WIDTH, HEIGHT);
+        setAlwaysOnTop(true);
+        userList.setListData(new String[]{"user1", "user2", "user3", "user4",
+                "user5", "user6", "user7", "user8", "user9",
+                "user-with-exceptionally-long-name-in-this-chat"});
+        JScrollPane scrUser = new JScrollPane(userList);
+        JScrollPane scrLog = new JScrollPane(log);
+        scrUser.setPreferredSize(new Dimension(100, 0));
+        log.setLineWrap(true);
+        log.setWrapStyleWord(true);
+        log.setEditable(false);
+        cbAlwaysOnTop.addActionListener(this);
+
+        btnSend.addActionListener(this);
+        tfMessage.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    log.insert(tfMessage.getText(), 0);
+                    // bufferedWriter.write(tfMessage.getText() + "\n"); // Не дошло, как имя пользователя добавить
+                    tfMessage.setText("");
+
+                }
+            }
+        });
+
+        panelTop.add(tfIPAddress);
+        panelTop.add(tfPort);
+        panelTop.add(cbAlwaysOnTop);
+        panelTop.add(tfLogin);
+        panelTop.add(tfPassword);
+        panelTop.add(btnLogin);
+        panelBottom.add(btnDisconnect, BorderLayout.WEST);
+        panelBottom.add(tfMessage, BorderLayout.CENTER);
+        panelBottom.add(btnSend, BorderLayout.EAST);
+
+        add(scrUser, BorderLayout.EAST);
+        add(scrLog, BorderLayout.CENTER);
+        add(panelTop, BorderLayout.NORTH);
+        add(panelBottom, BorderLayout.SOUTH);
+        setVisible(true);
+    }
+
+    /*void writerOn() throws IOException {
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter("log.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == cbAlwaysOnTop) {
+            setAlwaysOnTop(cbAlwaysOnTop.isSelected());
+        } else if (src == btnSend) {
+            log.insert(tfMessage.getText() + "\n", 0);
+            // запись в файл
+            tfMessage.setText("");
+        } else {
+            throw new RuntimeException("Unknown source:" + src);
+        }
+    }
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        e.printStackTrace();
+        String msg;
+        StackTraceElement[] ste = e.getStackTrace();
+        msg = String.format("Exception in \"%s\" %s: %s\n\tat %s",
+                t.getName(), e.getClass().getCanonicalName(), e.getMessage(), ste[0]);
+        JOptionPane.showMessageDialog(this, msg, "Exception", JOptionPane.ERROR_MESSAGE);
+        // закрыть reader
+        System.exit(1);
+    }
+}
